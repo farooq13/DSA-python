53051aba9bb0b7b0fb520653da39765b65db12a6
farooq13 <farukidris19@gmail.com>
2024-12-14 21:13:59 +0100

    stack

--- excercise.py
+++ excercise.py
@@ -1,5 +1,46 @@
-from array import *
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
 
-a = array('i', [1, 2, 3, 4, 5])
 
-print(a[::-1])
+class LinkedList:
+  def __init__(self):
+    self.head = None
+
+  def insertHead(self, newNode):
+    temporaryNode = self.head
+    self.head = newNode
+    self.head.next = temporaryNode
+    del temporaryNode
+
+  
+  def insertEnd(self, newNode):
+    if self.head is None:
+      self.head = newNode
+    else:
+      lastNode = self.head
+      while lastNode.next:
+        lastNode.next = newNode
+      lastNode.next = newNode
+
+  def printList(self):
+    currentNode = self.head
+    while currentNode:
+      print(currentNode.data)
+      currentNode = currentNode.next
+
+  
+
+firstNode = Node('Faruk')
+linkedList = LinkedList()
+linkedList.insertEnd(firstNode)
+
+secondNode = Node('Idris')
+linkedList.insertEnd(secondNode)
+
+
+thirdNode = Node('Ahmad')
+linkedList.insertHead(thirdNode)
+
+linkedList.printList()
---
+++ stack.py
@@ -0,0 +1,15 @@
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+
+print(browsing_session)
+
+last = browsing_session.pop()
+last = browsing_session.pop()
+last = browsing_session.pop()
+print(last)
+print(browsing_session)
+
+if not browsing_session:
+  print("Disabled")
