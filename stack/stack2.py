d10f8c7b87349ffc9252f306e9ed8335bc9c499a
farooq13 <farukidris19@gmail.com>
2024-12-14 21:54:14 +0100

    stack 2

---
+++ stack/stack2.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+
+class Stack:
+  def __init__(self):
+    self.container = deque()
+
+  def push(self, val):
+    self.container.append(val)
+
+  def pop(self):
+    return self.container.pop()
+  
+  def peek(self):
+    return self.container[-1]
+  
+  def is_empty(self):
+    return len(self.container) == 0
+  
+  def size(self):
+    return len(self.container)
+  
+  def __str__(self):
+    return f"Stack: {list(self.container)}"
+  
+
+s = Stack()
+s.push(5)
+s.push(4)
+s.push(3)
+print(s)
+
+s.pop()
+print(s)
+
+print(s)
