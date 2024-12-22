41654c7da3afe3d23f8abaee70ad00696c0f10f1
farooq13 <farukidris19@gmail.com>
2024-12-23 00:24:58 +0100

    bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,13 @@
-my_array = [30, 50, 23, 42, 5, 6, 8, 1, 2]
+my_array = [4, 53, 56, 23, 75, 33, 1, 5, 2]
 n = len(my_array)
 
 for i in range(n-1):
+  swapped = False
   for j in range(n-i-1):
     if my_array[j] > my_array[j+1]:
       my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+      swapped = True
+  if not swapped:
+    break
 
 print("Sorted Array: ", my_array)
